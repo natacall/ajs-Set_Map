5cea95ce6b68b9dfb7d29569c3513600f4668f38
Natali <shatabalova2011@gmail.com>
2023-02-18 08:52:59 +0700

    fix errors

--- src/js/map.js
+++ src/js/map.js
@@ -1,19 +1,22 @@
 export default class ErrorRepository {
-  constructor(errors = []) {
+  constructor() {
     this.errors = new Map();
-    errors.forEach(
-      (error) => {
-        const { code, description } = error;
-        this.add(+code, description);
-      },
-    );
-  }
-
-  add(code, description) {
-    this.errors.set(code, description);
+    this.errors.set(1, 'Описание ошибки с кодом 1');
+    this.errors.set(403, 'Forbidden');
+    this.errors.set(404, 'Not Found');
+    this.errors.set(408, 'Request Timeout');
+    this.errors.set(502, 'Bad Gateway');
+    this.errors.set(503, 'Service Unavailable');
+    this.errors.set(504, 'Gateway Timeout');
   }
 
   translate(code) {
-    return this.errors.get(code) || 'Unknown error';
+    if (!this.errors.has(code)) {
+      return 'Unknown error';
+    }
+
+    return this.errors.get(code);
   }
 }
+
+export const errorRepo = new ErrorRepository();
